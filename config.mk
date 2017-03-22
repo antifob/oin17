77ec3081adcf82fed9c6cde50feb8602efd8e5ac
Philippe Grégoire <gregoire.philippe.4@courrier.uqam.ca>
2017-03-21 22:52:35 -0400

    update config.mk and todo for h-c key

--- config.mk
+++ config.mk
@@ -14,13 +14,15 @@ PREFIX=		$${HOME}/rootfs
 CDBG=		-g
 CPPDBG=		-DOIN17_DEBUG=1
 #CPPREL=	-DOIN17_RELEASE
+#CPPKEY=	-DWALLET_REGISTERED
 
 CC=		gcc
 
 CFLAGS=		-pedantic -std=c99 -Wall -Wextra -Werror -O2 ${CDBG}
 
 # Do not touch _XOPEN_SOURCE
-CPPFLAGS=	-I${PREFIX}/include -D_XOPEN_SOURCE=700 ${CPPDBG} ${CPPREL}
+CPPFLAGS=	-I${PREFIX}/include -D_XOPEN_SOURCE=700 ${CPPDBG} ${CPPREL} \
+		${CPPKEY}
 
 LDFLAGS=	-Wl,-z,noexecstack -Wl,-z,now -Wl,-z,relro \
 		-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib ${CDBG}
